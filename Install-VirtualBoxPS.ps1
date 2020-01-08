911ab3f9640278e1bcb806db44a5f2ae3909f758
SmithersTheOracle <59446109+SmithersTheOracle@users.noreply.github.com>
2020-01-08 09:27:32 -0500

    Spelling

--- Install-VirtualBoxPS.ps1
+++ Install-VirtualBoxPS.ps1
@@ -2,12 +2,12 @@
 try{
  Write-Host '[INFO] Installing VirtualBox module'
  Write-Host '[INFO] Creating VirtualBox WebSrv startup task'
- if (((& cmd /c schtasks /query /fo csv /tn `"\Psuedo Services\VirtualBox\VirtualBox API Web Service`") | ConvertFrom-Csv).TaskName -ne '\Psuedo Services\VirtualBox\VirtualBox API Web Service') {
-  & cmd /c schtasks /create /ru `"SYSTEM`" /rp /tn `"\Psuedo Services\VirtualBox\VirtualBox API Web Service`" /XML `"$((Get-Location).Path)\VirtualBox API Web Service.xml`" | Write-Verbose
+ if (((& cmd /c schtasks /query /fo csv /tn `"\Pseudo Services\VirtualBox\VirtualBox API Web Service`") | ConvertFrom-Csv).TaskName -ne '\Pseudo Services\VirtualBox\VirtualBox API Web Service') {
+  & cmd /c schtasks /create /ru `"SYSTEM`" /rp /tn `"\Pseudo Services\VirtualBox\VirtualBox API Web Service`" /XML `"$((Get-Location).Path)\VirtualBox API Web Service.xml`" | Write-Verbose
  }
  else {Write-Host "[INFO] VirtualBox WebSrv startup task already exists"}
  Write-Host '[INFO] Starting VirtualBox WebSrv'
- if ((Test-Path -LiteralPath "$($env:VBOX_MSI_INSTALL_PATH)VBoxWebSrv.exe") -and !(Get-Process VBoxWebSrv)) {& cmd /c schtasks.exe /run /tn `"\Psuedo Services\VirtualBox\VirtualBox API Web Service`" | Write-Verbose}
+ if ((Test-Path -LiteralPath "$($env:VBOX_MSI_INSTALL_PATH)VBoxWebSrv.exe") -and !(Get-Process VBoxWebSrv)) {& cmd /c schtasks.exe /run /tn `"\Pseudo Services\VirtualBox\VirtualBox API Web Service`" | Write-Verbose}
  Write-Host '[INFO] Copying files'
  if (!(Test-Path "$($env:VBOX_MSI_INSTALL_PATH)sdk\bindings\webservice\")) {New-Item -ItemType Directory -Path "$($env:VBOX_MSI_INSTALL_PATH)sdk\bindings\webservice\" -Force -Confirm:$false | Write-Verbose}
  Copy-Item -Path "$((Get-Location).Path)\*.wsdl" -Destination "$($env:VBOX_MSI_INSTALL_PATH)sdk\bindings\webservice\" -Force -Confirm:$false | Write-Verbose
