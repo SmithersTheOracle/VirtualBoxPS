f92a1200a26ef7b0a8eeda363addf459207a75b9
SmithersTheOracle <59446109+SmithersTheOracle@users.noreply.github.com>
2020-01-09 17:32:53 -0500

    Add files via upload
    Basic sample of how to use VirtualBoxPS

---
+++ Sample_Script.ps1
@@ -0,0 +1,6 @@
+﻿$creds = Get-Credential -Message 'Enter credentials' -UserName $env:USERNAME
+Import-Module VirtualBoxPS -Verbose -Force -NoClobber
+Start-VirtualBoxSession -Credential $creds -Verbose
+Get-VirtualBoxVM -Verbose
+Get-VirtualBoxDisks -Verbose
+Pause
