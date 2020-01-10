6fb0dd0661d0ae78e656db7dee5b4a1676d8997c
SmithersTheOracle <59446109+SmithersTheOracle@users.noreply.github.com>
2020-01-09 20:43:35 -0500

    Add files via upload
    Added comments to the sample script

--- Sample_Script.ps1
+++ Sample_Script.ps1
@@ -1,6 +1,28 @@
-﻿$creds = Get-Credential -Message 'Enter credentials' -UserName $env:USERNAME
+﻿<##################################################
+# 
+#          VirtualBoxPS Sample Script
+# 
+# This is a sample script provided to give a basic
+# understanding of how to use the VirtualBoxPS
+# PowerShell module. For more detailed information
+# about this module use:
+#   Get-Module VirtualBoxPS -All
+# 
+# To get command related help import the module
+# and use Get-Help to for each command. Ex:
+#   Get-Help Get-VirtualBoxVM -Full
+# 
+##################################################>
+
+# get credentials to login to the VirtualBox Web Service
+$creds = Get-Credential -Message 'Enter credentials' -UserName $env:USERNAME
+# import the VirtualBoxPS module
 Import-Module VirtualBoxPS -Verbose -Force -NoClobber
+# login to the VirtualBox Web Service and create a global session ID
 Start-VirtualBoxSession -Credential $creds -Verbose
-Get-VirtualBoxVM -Verbose
-Get-VirtualBoxDisks -Verbose
+# get all VM information from the VirtualBox inventory
+Get-VirtualBoxVM -Verbose | Select-Object *
+# get all virtual disk information from the VirtualBox inventory
+Get-VirtualBoxDisks -Verbose | Select-Object *
+# pause the script and wait for user to press the Enter key before exiting
 Pause
